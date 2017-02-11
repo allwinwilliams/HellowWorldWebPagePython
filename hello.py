d7621b5db4e3ed23fab119b6ad9e6fd466c331a3
Allwin Williams <allwinwilliams.info@gmail.com>
2017-02-11 22:02:13 +0530

    added input from user as myRequest

--- hello.py
+++ hello.py
@@ -1,7 +1,20 @@
-from bottle import route, run
+from bottle import route, run, get, post, request # or route
 
 @route('/hello')
 def hello():
     return "<h1>Hello World!<h1>"
+    
+@get('/app')
+def app():
+    return '''
+        <form action="/app" method="post">
+            Input: <input name="myRequest" type="text" />
+        </form>
+    '''
 
-run(host='127.0.1.1',port=8080)
+@post('/app')
+def reply():
+    myRequest = request.forms.get('myRequest')
+    return app()+"request got: "+myRequest
+
+run(host='127.0.0.1',port=8081)
